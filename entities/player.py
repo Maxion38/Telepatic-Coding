bdc5ba1acfad2e4ceaa961f98627a498ed13dd02
abongartz <antoine.bongartz@student.uclouvain.be>
2026-01-27 12:41:43 +0100

    ** game.py ** - Added FULLSCREEN flag variable - Added fps as a Game's parameter
    ** Sprites **
    - Cropped the gecko-sprite
    
    ** player.py **
    - Added left and right arrows to move the player
    - Added a hitbox system (put the DEBUG flag variable to True to see it)
    - The lizard now face in the direction he's moving (he flips according to the hitbox)

--- entities/player.py
+++ entities/player.py
@@ -2,35 +2,86 @@ import pygame
 from pathlib import Path
 
 ASSETS = Path("assets/images")
+DEBUG = False
+
 
 class Player:
-    def __init__(self, pos):
+    def __init__(self, pos, sprite_name="gecko-sprite.png", scale=.33, hitbox="auto"):
         image = pygame.image.load(
-            ASSETS / "gecko-sprite.png"
+            ASSETS / sprite_name
         ).convert_alpha()
 
-        self.image = pygame.transform.scale(
-            image,
-            (256, 256)
-        )
+        # Rescale the image
+        self.image = pygame.transform.scale_by(image, scale)
 
+        # Rect representing the image
         self.rect = self.image.get_rect(center=pos)
+
+        # Rect representing the hitbox
+        if type(hitbox) == pygame.Rect:  # Custom hitbox
+            self.hitbox = hitbox
+            self.__hitbox_offset = hitbox.x * scale, hitbox.y * scale
+            self.hitbox = self.hitbox.scale_by(scale, scale)
+            self.hitbox.x, self.hitbox.y = self.rect.x, self.rect.y
+            self.hitbox.x += self.__hitbox_offset[0]
+            self.hitbox.y += self.__hitbox_offset[1]
+        else:                            # Default hitbox
+            self.hitbox = self.rect.copy()
+            self.__hitbox_offset = 0, 0
+
         self.speed = 600
 
         self.food = 100
-        self.food_decrease_speed = 2 # 2 per seconds
+        self.food_decrease_speed = 2  # 2 per seconds
 
+        self.__face_right = True
 
     def update(self, dt):
+
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_q]:
-            self.rect.x -= self.speed * dt
-        if keys[pygame.K_d]:
-            self.rect.x += self.speed * dt
+        # -- Player movement -- #
+
+        # Go left
+        if keys[pygame.K_q] or keys[pygame.K_LEFT]:
+            self.move(-self.speed * dt)
+        # Go right
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.move(self.speed * dt)
+
+        # -- Quit -- #
+        if keys[pygame.K_ESCAPE]:
+            return False
+
+        return True
 
         self.food -= dt * self.food_decrease_speed
         print(self.food)
 
+    def flip(self):
+        self.image = pygame.transform.flip(self.image, True, False)
+
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+        if DEBUG:
+            pygame.draw.rect(screen, (255, 255, 0), self.rect, 1)  # Show image rect
+            pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 1)  # Show hitbox
+
+    def move(self, amount):
+        if amount > 0:  # Move to the right
+            # Flip the player if needed
+            if not self.__face_right:
+                self.flip()
+                self.__face_right = True
+                self.rect.x -= self.__hitbox_offset[0]
+
+        else:  # Move to the left
+            # Flip the player if needed
+            if self.__face_right:
+                self.flip()
+                self.__face_right = False
+                self.rect.x += self.__hitbox_offset[0]
+
+        # Move the image and hitbox
+        self.rect.x += amount
+        self.hitbox.x += amount
--- scenes/game.py
+++ scenes/game.py
@@ -4,36 +4,43 @@ from pathlib import Path
 from .background import Background
 
 WALL_TEXTURES = Path("assets/images/wall-textures")
+FULLSCREEN = False
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, fps=60):
         pygame.init()
 
         init_width, init_height = 1200, 600
-        self.screen = pygame.display.set_mode(
-            (init_width, init_height),
-            pygame.FULLSCREEN
+
+        self.screen = (
+            pygame.display.set_mode((init_width, init_height), pygame.FULLSCREEN) if FULLSCREEN
+            else pygame.display.set_mode((init_width, init_height))
         )
 
         self.clock = pygame.time.Clock()
+        self.fps = fps
         self.running = True
 
         screen_rect = self.screen.get_rect()
-        self.player = Player(screen_rect.center)
+        self.player = Player(
+            pos=screen_rect.center,
+            sprite_name="gecko-sprite.png",
+            hitbox=pygame.Rect(252, 0, 249, 573),
+        )
 
         texture = pygame.image.load(WALL_TEXTURES / "BrokenWallA.png").convert()
         texture = pygame.transform.scale(texture, (512, 512))
         self.background = Background(texture, self.screen, speed=300)
 
         self.distance = 0
-        self.distance_increase_speed = 1 # 1 per second
+        self.distance_increase_speed = 1  # 1 per second
         self.end_distance = 100
 
 
     def run(self):
         while self.running:
-            dt = self.clock.tick(60) / 1000
+            dt = self.clock.tick(self.fps) / 1000
             self.handle_events()
             self.update(dt)
             self.draw()
@@ -48,7 +55,8 @@ class Game:
 
 
     def update(self, dt):
-        self.player.update(dt)
+        if not self.player.update(dt):
+            self.running = False
         self.background.update(dt)
 
 
